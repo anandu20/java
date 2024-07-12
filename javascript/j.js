9c3e095732b8a4c5c150dda3ca420461f452a4ba
anandu20 <ananduramachandran411@gmail.com>
2024-07-12 07:01:25 +0530

    javascript

---
+++ javascript/j.js
@@ -0,0 +1,42 @@
+// a=0;
+// sum=0;
+    // do
+    // {
+    //     if(a%2==1)
+    //         {
+    //             console.log( "The odd numbers are "+a);
+    //         }
+    //     a++;
+        
+    // }while(a<=10);
+
+    // for(a=0;a<=10;a++)
+
+    //     {
+    //        sum=sum+a;
+            
+    //     }
+    //     console.log(sum);
+    
+
+    // for(a=1;a<=10;a++)
+
+    //     {
+    //         if(a%2==0)
+    //             {
+    //                 console.log("The even numbers are "+a);
+    //             }
+            
+    //     }
+    
+    let a=0;
+    let b=1;
+    let c;
+    console.log(a);
+    console.log(b);
+    for(let i=0;i<=10;i++){
+        c=a;
+        a=b;
+        b=a+c;
+        console.log(b);
+    }
