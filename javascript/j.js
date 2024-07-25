1c6284f87f4c5bd6cc599752870329e1cd0b2e8b
anandu20 <ananduramachandran411@gmail.com>
2024-07-25 13:36:26 +0530

    jbhege

--- javascript/j.js
+++ javascript/j.js
@@ -29,14 +29,15 @@
             
     //     }
     
-    let a=0;
-    let b=1;
-    let c;
-    console.log(a);
-    console.log(b);
-    for(let i=0;i<=10;i++){
-        c=a;
-        a=b;
-        b=a+c;
-        console.log(b);
-    }
+    // let a=0;
+    // let b=1;
+    // let c;
+    // console.log(a);
+    // console.log(b);
+    // for(let i=0;i<=10;i++){
+    //     c=a;
+    //     a=b;
+    //     b=a+c;
+    //     console.log(b);
+    // }
+    hgv
